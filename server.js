667c41406bc5bdf4fd93cf491802401cc28b2682
Leoozz69 <164828114+Leoozz69@users.noreply.github.com>
2024-10-05 03:48:49 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mercadopago = require('mercadopago');
 const path = require('path');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors'); // Adicionado para permitir CORS
 
 const app = express();
 const server = http.createServer(app);
@@ -16,6 +17,7 @@ mercadopago.configurations.setAccessToken('APP_USR-6293224342595769-100422-59d0a
 // Middleware para servir arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(cors()); // Habilitando CORS
 
 // Rota para criar o pagamento e gerar o QR code PIX
 app.post('/generate_pix_qr', (req, res) => {
@@ -44,7 +46,6 @@ app.post('/generate_pix_qr', (req, res) => {
 
   mercadopago.payment.create(payment_data)
     .then(function (response) {
-      // Verificar se o campo point_of_interaction está disponível
       const point_of_interaction = response.body.point_of_interaction;
 
       if (point_of_interaction && point_of_interaction.transaction_data) {
@@ -62,29 +63,7 @@ app.post('/generate_pix_qr', (req, res) => {
     });
 });
 
-// Rota para receber notificações de pagamento do Mercado Pago
-app.post('/notifications', (req, res) => {
-  const paymentId = req.body.data.id;
-
-  mercadopago.payment.findById(paymentId)
-    .then(function (response) {
-      const paymentStatus = response.body.status;
-
-      if (paymentStatus === 'approved') {
-        // Emitir evento para confirmar pagamento
-        io.emit('paymentApproved');
-        console.log('Pagamento aprovado!');
-      }
-
-      res.sendStatus(200);
-    })
-    .catch(function (error) {
-      console.error('Erro ao processar notificação:', error);
-      res.sendStatus(500);
-    });
-});
-
-// Inicializa o servidor
+// Iniciar o servidor
 server.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
